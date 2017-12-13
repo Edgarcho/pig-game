8b4f84a6606a169e64c31e13035df606d4580b5b
Edgar Hernandez and Zach Evans <zach@>
2017-12-13 15:30:04 -0800

    add prototype to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,38 @@
+var objArray = [];
+
 function Player(name) {
   this.playerName = name;
   this.playerTotal = 0;
   this.playerScore = [];
-  this.turn = false;
-}
-
-Player.prototype.addScore = function(numbers) {
-  for (i=0 ; i=this.playerScore.length - 1; i++) {
-    var sum = sum + this.playerScore[i]
-  }
+  this.playerTurn = 0;
 }
 
 Player.prototype.game = function(number) {
-  function getRandomNumber(min, max) {
-    return Math.round(Math.random() * (max - min) + min);
+  if (objArray[0].playerTurn === objArray[1].playerTurn) {
+    var roll = Math.floor(Math.random() * 6+1);
+      if (roll === 1) {
+        this.playerScore = [];
+        objArray[0].playerTurn += 1;
+      } else if (roll > 1) {
+        this.playerScore.push(roll);
+        console.log(this.playerScore);
+      } else {
+
+        } //close inside if
+      } else {
+
+      } //close outside if
+    } //close prototype
+
+Player.prototype.addScore = function(numbers) {
+  if (objArray[0].playerTurn === objArray[1].playerTurn) {
+  var sum = 0;
+
+  for (i=0 ; i<=this.playerScore.length - 1; i++) {
+    sum = sum + this.playerScore[i]
   }
-
-  var roll = getRandomNumber(1,6);
-  // console.log(roll);
-  //console.log(newPlayer);
-  if (roll === 1) {
-    this.playerScore = 0;
-  } else if (roll > 1) {
-    this.playerScore.push(roll);
-    console.log(this.playerScore);
-  } else {
-
+  this.playerTotal = sum;
+    objArray[0].playerTurn += 1;
   }
 }
 
@@ -34,16 +41,22 @@ $(document).ready(function(){
   $("#playerInfo").submit(function(event){
     event.preventDefault();
     var playerOne = $("#player-one").val();
-    //var playerTwo = $("#player-two").val();
-    newPlayer = new Player(playerOne);
-    console.log(newPlayer);
+    var playerTwo = $("#player-two").val();
+    newPlayerOne = new Player(playerOne);
+    newPlayerTwo = new Player(playerTwo);
+    objArray.push(newPlayerOne);
+    objArray.push(newPlayerTwo);
+    console.log(objArray);
   });
   $("#formOne").submit(function(event){
     event.preventDefault();
-    console.log(newPlayer.playerScore);
-    newPlayer.game();
-    console.log(newPlayer.playerScore);
-    // game();
-    // console.log();
+    newPlayerOne.game();
+    newPlayerTwo.game();
+  });
+  $("#hold").click(function(event) {
+    newPlayerOne.addScore();
+    newPlayerTwo.addScore();
+    console.log(newPlayerOne.playerTotal);
+    console.log(newPlayerTwo.playerTotal);
   });
 });
